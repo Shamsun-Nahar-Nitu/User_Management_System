64b3e177d5e31498812865207d5ffd5401fd8d8f
nitu <124525218+Shamsun-Nahar-Nitu@users.noreply.github.com>
2026-07-15 11:12:23 +0600

    Enhance user dashboard with profile details and improve login/signup templates

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic.edit import CreateView
 from .forms import CustomSignUpForm
 
-class SignUpView(generic.CreateView):
+class SignUpView(CreateView):
     form_class = CustomSignUpForm
     success_url = reverse_lazy('login') 
     template_name = 'registration/signup.html'
